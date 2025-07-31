513b7ebe4258f56393ca31cc018dfe7b356af355
Carlos <whitecircuit3000@gmail.com>
2025-07-31 08:51:19 -0600

    se agrega el main, solo sirve para ejecutar la interfaz

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-"""aqui ira la union entre la logica y la interfaz"""
+from ui.app import TruthTableApp
+
+def main():
+    """
+    ahora si ya corre la app
+    """
+    app = TruthTableApp()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
